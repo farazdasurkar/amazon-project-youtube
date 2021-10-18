7f3c9503423fcdd82c2fe8486611c0c94c8c8729
farazdasurkar <farazdasurkar@users.noreply.github.com>
2021-10-18 23:31:44 +0530

    commit

--- amazyou/src/Checkout.js
+++ amazyou/src/Checkout.js
@@ -5,19 +5,21 @@ function Checkout() {
     return (
         <div className="checkout">
             <div className="checkout--left">
-                <div>
-                    <h3 className="checkout--title">Your Shopping Details </h3>
-
-                </div>
+                <img className="checkout--adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png"></img>
+            </div>
+            <div>
+                <h3 className="checkout--title">Your Shopping Details </h3>
 
             </div>
+
+
 
             <div className="checkout--right">
 
                 <h3>SubTotal (2 item):</h3>
 
             </div>
-        </div>
+        </div >
     )
 }
 
